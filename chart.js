cfd90555d25cc6b607266d0c5e80766d8c41f3a0
thirawr <tallyndavis@comcast.net>
2015-09-18 14:14:46 -0500

    Simple D3 barchart

---
+++ chart.js
@@ -0,0 +1,18 @@
+var data = [4, 8, 15, 16, 23, 42, 60, 39];
+
+function sortNumber(a, b){
+	return a - b;
+}
+
+data.sort(sortNumber);
+
+var scale = d3.scale.linear()
+	.domain([0, d3.max(data)])
+	.range([0,600]);
+
+d3.select(".chart")
+	.selectAll("div")
+		.data(data)
+	.enter().append("div")
+		.style("width", function(d){ return scale(d) + "px"; })
+		.text(function(d) { return d; });
